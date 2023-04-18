7f4a9f0f4182c2150f081d5680e39d671c952465
surajkeshari <surajkeshari.knp2019@gmail.com>
2023-04-19 03:05:14 +0530

    calendar added in navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,11 +5,21 @@ import { faPlane } from "@fortawesome/free-solid-svg-icons"
 import { faCar } from "@fortawesome/free-solid-svg-icons"
 import { faCalendarDays } from "@fortawesome/free-solid-svg-icons"
 import { faPerson  } from "@fortawesome/free-solid-svg-icons"
-
+import { DateRange } from 'react-date-range';
+import {useState} from 'react'
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 // import { faBed } from "@fortawesome/free-solid-svg-icons"
 import { faTaxi } from "@fortawesome/free-solid-svg-icons"
 const Header = () => {
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: 'selection'
+    }
+  ]);
   return (
     <div className="header">
     <div className="headerContainer">
@@ -52,6 +62,12 @@ const Header = () => {
       <div className="headerSearchItem">
         <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
        <span className="headerSearchText">date to date </span>
+       <DateRange
+  editableDateInputs={true}
+  onChange={item => setDate([item.selection])}
+  moveRangeOnFirstSelection={false}
+  ranges={date}
+/>
       </div>
 
       <div className="headerSearchItem">
